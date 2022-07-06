ee30b9826f42269d322521a7612cf7ca32285033
hongyan <625293627@qq.com>
2022-07-06 14:44:06 +0800

    1

---
+++ services/ordering/src/EShopOnAbp.OrderingService.HttpApi.Host/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using EShopOnAbp.OrderingService.Orders;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShopOnAbp.OrderingService.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<TestController> logger;
+        private readonly IOrderRepository orderRepository;
+
+        public TestController(IConfiguration configuration, ILogger<TestController> logger, IOrderRepository orderRepository)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+            this.orderRepository = orderRepository;
+        }
+
+        [HttpGet]
+        [Route("postgre")]
+        public async Task<IActionResult> Postgre()
+        {
+            var orderingServiceConnectionString = this.configuration["ConnectionStrings:OrderingService"];
+            this.logger.LogWarning($"OrderingServiceConnectionString:{orderingServiceConnectionString}");
+            var orderList = await this.orderRepository.GetListAsync();
+
+
+            return Ok(new { OrderList=orderList.Select(o=>new { id=o.Id,orderno=o.OrderNo,orderdate=o.OrderDate}) });
+        }
+    }
+}
